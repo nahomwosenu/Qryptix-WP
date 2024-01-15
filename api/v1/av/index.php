d69056b616c39780bdcf551cc1e8154b35e64ce5
Nahom W <alphageekict@gmail.com>
2024-01-15 20:36:07 +0300

    php downgrade

--- api/v1/av/index.php
+++ api/v1/av/index.php
@@ -1,5 +1,8 @@
 <?php
 header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Authorization");
 require_once __DIR__."/../../../util/VulScanner.php";
 if(isset($_GET['scan']) && isset($_GET['domain'])){
     $domain=$_GET['domain'];
