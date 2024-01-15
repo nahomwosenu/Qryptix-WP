0eaeed509b91ebf5962a6bacb62bc20cabd5343f
Nahom W <alphageekict@gmail.com>
2024-01-15 19:35:31 +0300

    added offline cve db

---
+++ api/v1/av/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__."/../../../util/VulScanner.php";
+if(isset($_GET['scan']) && isset($_GET['domain'])){
+    $domain=$_GET['domain'];
+    $scan=new VulScanner($domain);
+    $scan->startScan();
+    $result=$scan->getResult();
+    $r=array();
+    $r['status']='success';
+    $r['data']=$result;
+    die(json_encode($r));
+}
