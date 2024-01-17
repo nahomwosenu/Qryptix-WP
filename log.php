06c53b8c4221704c2605d2c36aaf234237dc372a
Nahom W <alphageekict@gmail.com>
2024-01-17 19:26:02 +0300

    added logger

--- log.php
+++ log.php
@@ -8,3 +8,8 @@ $inputData=file_get_contents('php://input') ?? "";
 
 $logData="time: ".date('y-m-d h:i')."\r\nSource: ".$requestFrom."\r\nRequest: ".$request."\r\nInputData: ".$inputData."\r\n\r\n\r\n";
 file_put_contents(__DIR__.'/log.txt',$logData,FILE_APPEND);
+
+function d($d){
+    $logData="time: ".date('y-m-d h:i')."\r\nSource: SRC_SCRIPT\r\nRequest: TASK_SCANNER\r\nLog: ".$d."\r\n\r\n\r\n";
+    file_put_contents(__DIR__.'/log.txt',$logData,FILE_APPEND);
+}
--- scripts/scanner-task.php
+++ scripts/scanner-task.php
@@ -2,6 +2,7 @@
 <?php
 require_once __DIR__."/../model/Cache.php";
 require_once __DIR__."/../util/VulScanner.php";
+include_once __DIR__."/../log.php";
 $CACHE_KEY="av_result";
 unset($argv[0]);
 parse_str(implode('&',$argv), $_REQUEST);
@@ -11,8 +12,9 @@ $domain=$_REQUEST['domain'];
 
 $cmd="nmap -sV --script=vulscan/vulscan.nse $domain";
 $result= shell_exec($cmd);
-
-$scanner=new VulScanner($domain);
+$l="CMD: $cmd \r\n client & domain: $clientId,$domain \r\n Result: ".$result;
+d($l);
+$scanner=new VulScanner($domain,$clientId);
 $scanner->rawResult=$result;
 $scanner->parse();
 $data=$scanner->getResult();
